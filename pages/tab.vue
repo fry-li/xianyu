2de7badb529a301d81b52e706060b76d21254ef2
麋鹿 <1669277128@qq.com>
2019-09-01 15:49:09 +0800

    闲云旅游项目初始化

---
+++ pages/tab.vue
@@ -0,0 +1,62 @@
+<template>
+  <div style="padding-left:60px;margin-bottom:20px;">
+    <div class="tab">
+      <span
+        v-for="(item,index) in ['体育','娱乐','生活']"
+        :key="index"
+        @click="handleClick(index)"
+        :class="{ active: current === index }"
+      >{{item}}</span>
+    </div>
+    <div v-show="current === 0">
+      <ul>
+        <li>科比退役</li>
+        <li>科比退役</li>
+        <li>科比退役</li>
+      </ul>
+    </div>
+    <div v-show="current ===1">
+      <ul>
+        <li>周柏豪中山演唱会临时取消</li>
+        <li>周柏豪中山演唱会临时取消</li>
+        <li>周柏豪中山演唱会临时取消</li>
+      </ul>
+    </div>
+    <div v-show="current ===2">
+      <ul>
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 0
+    };
+  },
+  methods: {
+    handleClick(index) {
+      this.current = index;
+    }
+  }
+};
+</script>
+
+<style lang="less" scope>
+.tabs {
+  margin-bottom: 20px;
+
+  span {
+    cursor: pointer;
+  }
+}
+
+.active {
+  color: red;
+}
+</style>
