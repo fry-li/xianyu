7fb9df605441288567407279b04f68235faab463
麋鹿 <1669277128@qq.com>
2019-09-02 09:17:20 +0800

    头部组件完成

---
+++ components/header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <el-row type="flex" class="main" justify="space-between">
+      <!-- logo -->
+      <div class="logo">
+        <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+      </div>
+      <!-- 头部导航 -->
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <!-- 登录 -->
+      <div class="login">
+        <nuxt-link to="/user/login">登录/注册</nuxt-link>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.container {
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px #ddd solid;
+  box-shadow: 0px 2px 2px #ddd;
+}
+.main {
+  width: 1000px;
+  margin: 0 auto;
+}
+.navs {
+  flex: 1;
+  margin-left: 10px;
+  a{
+      display: block;
+      height: 60px;
+      padding: 0 20px;
+      box-sizing:border-box;
+      &:hover{
+          color: lawngreen;
+          border-bottom:5px lawngreen solid;
+      }
+  }
+  .nuxt-link-exact-active{
+          background:lawngreen;
+          color:#fff;
+        &:hover{
+          color:#fff;
+        }
+      }
+}
+.logo {
+    padding-top:10px;
+  img{
+      width: 156px;
+      height: 42px;
+      display: block;
+  }
+}
+</style>
