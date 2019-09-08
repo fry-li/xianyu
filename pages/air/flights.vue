685e9c4b01900df0409e3321da6f2584294ed1de
麋鹿 <1669277128@qq.com>
2019-09-08 11:44:46 +0800

    动态生成机票列表与渲染

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,13 +1,71 @@
 <template>
-  <div>机票列表首页</div>
+   <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+
+                </div>
+
+                <!-- 航班头部布局 -->
+                <FlightsListHead/>
+
+                <div>
+                <!-- 航班信息 -->
+               <FlightsItem 
+                    v-for="(item, index) in flightsData.flights"
+                    :key="index"
+                    :data="item"/>
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
+import FlightsListHead from "@/components/air/flightsListHead.vue"
+import FlightsItem from "@/components/air/flightsItem"
 export default {
-
+    data(){
+        return{
+            flightsData:{}
+        }
+    },
+    components:{
+        FlightsListHead,
+        FlightsItem
+    },
+    mounted(){
+        this.$axios({
+            url:'airs',
+            params:this.$route.query
+        })
+        .then(res => {
+            console.log(res)
+            // 赋值给总数据
+            this.flightsData = res.data;
+        })
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+    .aside{
+        width:240px;
+    } 
 </style>
